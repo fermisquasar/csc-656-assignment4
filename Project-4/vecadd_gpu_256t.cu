052d793d27d792acc439d3611da48dae674800c0
fermisquasar <bullard.elliott@gmail.com>
2024-05-16 19:26:03 -0700

    update

---
+++ Project-4/vecadd_gpu_256t.cu
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+//Add two vectors
+__global__
+void add(int n, float *x, float *y){
+    int index = threadIdx.x;
+    int stride = blockDim.x;
+    for (int i = index; i < n; i += stride){
+        y[i] = x[i] + y[i];
+    }
+}
+int main(void) {
+    const int N = 1 << 29;
+
+    // Allocate Unified Memory -- accessible from CPU or GPU
+    float *x, *y;
+    cudaMallocManaged(&x, N*sizeof(float));
+    cudaMallocManaged(&y, N*sizeof(float));
+
+    //Initialize x and y arrays
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    //Vector addition
+    add<<<1, 256>>>(N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+
+//    std::cout << "Time passed: " << duration_ms.count() << " ms\n";
+
+    //Check results
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++) {
+        maxError = fmax(maxError, fabs(y[i]-3.0f));
+    }
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
